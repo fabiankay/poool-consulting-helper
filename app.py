06ecf8feb567cbd56f68ee0a59b6fb53bdd798cb
Fabian Kainz <fabian.kainz@poool.cc>
2025-11-10 16:09:51 +0100

    update crm type conversion

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ crm_update = st.Page("sites/crm_update.py", title="CRM Aktualisierung", icon="�
 person_update = st.Page("sites/person_update.py", title="Personen Aktualisierung", icon="👤")
 
 # Tools pages
-stundensatz = st.Page("sites/cost_calculator.py", title="Stundensatzkalkulation", icon="📊")
+# stundensatz = st.Page("sites/cost_calculator.py", title="Stundensatzkalkulation", icon="📊")
 personio = st.Page("sites/personio.py", title="Personio", icon="🧑‍💻")
 
 # Analytics pages
@@ -22,7 +22,7 @@ clustering = st.Page("sites/clustering.py", title="Clustering", icon="🔍")
 pg = st.navigation({
     "Allgemein": [home],
     "CRM Operationen": [crm_import, crm_update, person_update],
-    "Tools": [stundensatz, personio]
+    "Tools": [personio]
 })
 
 pg.run()
